c5f6e4e31e2b44b8bd5f98f4aff6fe582de3189f
awroda596 <awroda596@gmail.com>
2024-04-11 16:47:39 -0700

    firebase initial implementation

--- real_e_sign/.dart_tool/dartpad/web_plugin_registrant.dart
+++ real_e_sign/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -9,6 +9,7 @@
 import 'package:desktop_webview_auth/desktop_webview_auth_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
@@ -16,5 +17,6 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   DesktopWebviewAuthWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
