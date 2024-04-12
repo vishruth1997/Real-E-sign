ef6d2070ca9bd73f115ac7a77e93ec9cf582ec47
aroda596 <awroda596@gmail.com>
2024-04-11 20:48:45 -0700

    update from main

--- real_e_sign/.dart_tool/dartpad/web_plugin_registrant.dart
+++ real_e_sign/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,11 +6,19 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:desktop_webview_auth/desktop_webview_auth_web.dart';
 import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  DesktopWebviewAuthWeb.registerWith(registrar);
   FilePickerWeb.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
