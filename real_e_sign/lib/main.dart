da03560abab635cd04528cf2b3ef65620d713ab1
awroda596 <awroda596@gmail.com>
2024-05-13 13:10:50 -0700

    fix import

--- real_e_sign/lib/main.dart
+++ real_e_sign/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/Login.dart';
+import 'pages/LogIn.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
